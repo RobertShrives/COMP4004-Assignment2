d17b48bb8011d3cceda34305628c6c5fea21129a
Rob Shrives <robshrives@gmail.com>
2017-11-02 01:11:18 -0400

    Project initialization

---
+++ SimpleLibrary/src/cucumberTests/CucumberRunner.java
@@ -0,0 +1,14 @@
+package cucumberTests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin={ "pretty", "html:output/cucumber.html"}
+		)
+public class CucumberRunner {
+
+}
